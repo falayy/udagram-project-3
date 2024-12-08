275ff2c2dec688ff9414ad3e2febc6039afbe0df
corneliuze <falaycornelius@gmail.com>
2024-12-08 11:18:53 +0100

    separated into micro services, add nginx, created docker images

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=falay
+export POSTGRES_PASSWORD=m6543210
+export POSTGRES_HOST=udagramprojectdb.c3kccqo8wkwq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project-847129622644-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
