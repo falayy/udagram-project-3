9a96e1f8883e868dd42941f620d942f4508d555a
corneliuze <falaycornelius@gmail.com>
2025-01-19 00:09:43 +0100

    updates rds and bucker names

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=falay
 export POSTGRES_PASSWORD=Password1998
-export POSTGRES_HOST=database-1.cuwdgcdxebvj.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udagram.c0hauzk3yoil.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=udagram-962289618667-bucket
+export AWS_BUCKET=udagram-486157676454
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
